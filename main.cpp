a2efa929a9d2881d8592aee70a3eae162ed90b9b
Gabriel Antonio <gmav1010@gmail.com>
2020-05-14 08:03:24 -0300

    Leitura dos outros dados

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 
 using namespace std;
 int main() {
-	int escolaridade;
+	int escolaridade, experiencia;
+	string viaja, habilitacao;
+	
 	cout << "Qual sua escolaridade?" << endl;
 	cout << "1) Ensino Fundamental" << endl;
 	cout << "2) Ensino Medio" << endl;
@@ -10,6 +12,12 @@ int main() {
 	cout << "4) Pos-graduacao" << endl;
 	cout << "Digite uma opcao: ";
 	cin >> escolaridade;
+	cout << endl << "Voce tem quantos anos de experiencia profissional? ";
+	cin >> experiencia;
+	cout << "Voce tem disponibilidade para viajar (S/N)?";
+	cin >> viaja;
+	cout << "Voce tem habilitacao de motorista (S/N)?";
+	cin >> habilitacao;
 	
 	return 0;
 }
