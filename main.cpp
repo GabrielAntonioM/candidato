4764b93f797c923c0c9dd3ad762ef5f18ab04f65
Gabriel Antonio <gmav1010@gmail.com>
2020-05-14 08:18:18 -0300

    Escolaridade e experiencia

--- main.cpp
+++ main.cpp
@@ -14,10 +14,31 @@ int main() {
 	cin >> escolaridade;
 	cout << endl << "Voce tem quantos anos de experiencia profissional? ";
 	cin >> experiencia;
-	cout << "Voce tem disponibilidade para viajar (S/N)?";
+	cout << "Voce tem disponibilidade para viajar (S/N)? ";
 	cin >> viaja;
-	cout << "Voce tem habilitacao de motorista (S/N)?";
+	cout << "Voce tem habilitacao de motorista (S/N)? ";
 	cin >> habilitacao;
+	int somaexp, somaesc;
+	if(escolaridade == 1){
+		somaesc = 10;
+	}else if(escolaridade == 2){
+		somaesc = 20;
+	}else if(escolaridade == 3){
+		somaesc = 30;
+	}else{
+		somaesc = 40;
+	}
+	cout << endl << "Pontos por escolaridade: " << somaesc << endl;
+	if(experiencia <= 0){
+		somaexp = 0;
+	}else if(experiencia <= 2){
+		somaexp = 10;
+	}else if(experiencia <= 5){
+		somaexp = 20;
+	}else{
+		somaexp = 40;
+	}
+	cout << "Pontos por experiencia: " << somaexp << endl;
 	
 	return 0;
 }
